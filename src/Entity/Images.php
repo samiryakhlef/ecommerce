db1e921aaec8d3555972e1081a1a20ca9ea62443
samir_yakhlef <samir.yakhlef30@gmail.com>
2022-03-12 11:54:29 +0100

    mise en place de la BDD

---
+++ src/Entity/Images.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImagesRepository::class)]
+class Images
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\ManyToOne(targetEntity: products::class, inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $products;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProducts(): ?products
+    {
+        return $this->products;
+    }
+
+    public function setProducts(?products $products): self
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+}
